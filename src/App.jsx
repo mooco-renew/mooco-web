fc35a157141529fbff6c6e62b8f14e3c34afe426
eunji <bian87@dgu.ac.kr>
2024-04-04 23:47:17 +0900

    fix: globalstyle 위치 수정, route 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import GlobalStyles from './style/globalstyle'
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './screen/main/MainPage';
 
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
-    <>
-      <GlobalStyles />
-      </>
+    <Routes>
+    <Route path='/' element={<MainPage />} >
+    </Route>
+  </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import GlobalStyles from './style/globalstyle.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <GlobalStyles />
     <App />
     </BrowserRouter>
 )
