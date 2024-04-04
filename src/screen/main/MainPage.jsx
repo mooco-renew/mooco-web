691ca180818f3a7f6a9c632b1ad7fc78d7900462
eunji <bian87@dgu.ac.kr>
2024-04-05 00:23:50 +0900

    style : main 온보딩용 mooco 로고 이미지 추가

--- src/screen/main/MainPage.jsx
+++ src/screen/main/MainPage.jsx
@@ -1,10 +1,13 @@
-import { Container } from '../../style/style'
-import Mooco  from '../../assets/main/mooco.svg';
+import { Container } from '../../style/style';
+import Mooco  from '/src/assets/main/mooco.svg';
+import { Body } from '/src/style/style';
 
 export default function MainPage() {
   return (
+    <Body>
       <Container>
         <img src={Mooco} />
-      </Container>
+        </Container>
+        </Body>
   )
 }
--- src/style/style.jsx
+++ src/style/style.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Body = styled.div`
 height: calc(var(--vh, 1vh) * 100);
 background-color: #000000;
 width: 100%;
+aligh-items: center;
 
 // 모바일 뷰를 위한 스타일
 @media (max-width: 768px) {
@@ -12,4 +13,12 @@ width: 100%;
 // PC 뷰를 위한 스타일
 @media (min-width: 769px) {
 }
+`
+
+export const Container = styled.div`
+width: 100%;
+height: 100%;
+display: flex; 
+align-items: center; 
+justify-content: center; 
 `
