abddb71e39c222a9587381d038fd7ece41301170
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-04-08 16:06:59 +0900

    fix : 문법 오류

--- src/screen/create-barcord/createButton.jsx
+++ src/screen/create-barcord/createButton.jsx
@@ -35,9 +35,9 @@ export default function CreateButtonPage({buttonRef, pictureRef, scrollToRef, na
         alert("server error");
         navigate('/');
       }
-  } else if(
+  } else {
     setErrorMessage(true);
-  )
+  }
   }
 
   return (
