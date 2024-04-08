38088e53cece4c45d9a0b9de1090f26d2456b07b
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-04-09 02:09:49 +0900

    comment : 주석 다시 확인

--- src/api/barcord/sendImages.js
+++ src/api/barcord/sendImages.js
@@ -1,39 +1,39 @@
 import axios from "axios";
 
 export const handleSubmit = async (files) => {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    for (const file of files) {
-      formData.append('photos[]', file);
-    }
-    
-    // FormData 내용 콘솔에 출력 (디버깅 목적)
-    for (const pair of formData.entries()) {
-      console.log(`entrys : ${pair[0]}: ${pair[1]}`);
-    }
+  for (const file of files) {
+    formData.append('photos[]', file);
+  }
 
-    try {
-      // 서버에 POST 요청
-      let response = await axios.post(
-        `${import.meta.env.VITE_APP_SERVER_HOST}/web/barcode`,
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        }
-      );
-      console.log('server-host : ', import.meta.env.VITE_APP_SERVER_HOST);
+  // FormData 내용 콘솔에 출력 (디버깅 목적)
+  for (const pair of formData.entries()) {
+    console.log(`entrys : ${pair[0]}: ${pair[1]}`);
+  }
 
-      if (response.data.success === true) {
-        console.log('이미지 불러오기 성공!', response.data);
-        return response.data;
-      } else {
-        console.log('이미지 불러오기 실패!', response.data);
-        return response.data;
+  try {
+    // 서버에 POST 요청
+    let response = await axios.post(
+      `${import.meta.env.VITE_APP_SERVER_HOST}/web/barcode`,
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
       }
-    } catch (error) {
-        console.log('통신 에러!', error);
-        return error;
+    );
+    console.log('server-host : ', import.meta.env.VITE_APP_SERVER_HOST);
+
+    if (response.data.success === true) {
+      console.log('이미지 불러오기 성공!', response.data);
+      return response.data;
+    } else {
+      console.log('이미지 불러오기 실패!', response.data);
+      return response.data;
     }
-  };
+  } catch (error) {
+    console.log('통신 에러!', error);
+    return error;
+  }
+};
