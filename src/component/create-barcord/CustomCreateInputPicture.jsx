7aa6d64b37c97feaa9ac3865d1cfd5463d03f26b
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-04-09 14:07:45 +0900

    fix : 이미지 파일 크기 조절

--- src/component/create-barcord/CustomCreateInputPicture.jsx
+++ src/component/create-barcord/CustomCreateInputPicture.jsx
@@ -1,18 +1,45 @@
 import React, { useState, useRef } from 'react';
 import { FaCamera, FaTimesCircle  } from 'react-icons/fa'; // react-icons에서 카메라 아이콘 가져오기
 import "/src/component/create-barcord/CustomCreateValue.css";
+import Resizer from 'react-image-file-resizer';
 
 export default function CustomCreateInputPicture({images, setImages, files, setFiles}) {
   const fileInputRef = useRef(null); // 파일 입력을 위한 ref 생성
+  const DISK_IMG_MAX_LENGTH = 120;
+  const IMG_MAX_SIZE = 10000000;
 
-  const handleImageChange = (e) => {
-    const forImagesArray = Array.from(e.target.files);
-    const newImages = forImagesArray.map((file) =>
-      URL.createObjectURL(file)
-    );
-    setImages([...images, ...newImages]);
-    setFiles([...files, ...e.target.files]);
+  const handleImageChange = async (e) => {
+    const filesArray = Array.from(e.target.files);
+    
+    if (filesArray.length + images.length > DISK_IMG_MAX_LENGTH) {
+      window.alert(`${DISK_IMG_MAX_LENGTH}개까지 이미지를 업로드할 수 있습니다.`);
+      return;
+    }
+  
+    for (let file of filesArray) {
+      if (file.size > IMG_MAX_SIZE) {
+        window.alert(`${Math.round(IMG_MAX_SIZE / 1000000)}MB 보다 작은 이미지만 업로드할 수 있습니다.`);
+      } else {
+        try {
+          // 이미지 파일의 크기를 조정
+          const resizedImage = await new Promise((res, rej) => {
+            Resizer.imageFileResizer(file, 1500, 1500, "JPEG", 80, 0, (uri) => {
+              res(uri);
+            }, "file");
+          });
+  
+          // 리사이징된 이미지를 미리보기로 상태 업데이트
+          const newImageURL = URL.createObjectURL(resizedImage);
+          setImages((prevImages) => [...prevImages, newImageURL]);
+          setFiles((prevFiles) => [...prevFiles, resizedImage]);
+        } catch (error) {
+          window.alert("이미지를 불러오는 데 실패했습니다.");
+          console.error(error);
+        }
+      }
+    }
   };
+  
 
     // 이미지 삭제 함수
     const removeImage = (event, index) => {
